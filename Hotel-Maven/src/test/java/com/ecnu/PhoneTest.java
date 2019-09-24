3a91aceda94a8fd1e25d43997548048a62eb85e6
yueyue200830 <45792349+yueyue200830@users.noreply.github.com>
2019-09-25 00:57:42 +0800

    Use mockito to test

--- Hotel-Maven/src/test/java/com/ecnu/PhoneTest.java
+++ Hotel-Maven/src/test/java/com/ecnu/PhoneTest.java
@@ -3,33 +3,33 @@ package com.ecnu;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 import java.util.Calendar;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class PhoneTest {
-    Phone phone;
-    Clock[] clocks;
-
-    @BeforeEach
-    void setUp() {
-        clocks = new Clock[5];
-        clocks[0] = new Clock(8, "Beijing");
-        clocks[1] = new Clock(0, "London");
-        clocks[2] = new Clock(-4, "Moscow");
-        clocks[3] = new Clock(10, "Sydney");
-        clocks[4] = new Clock(-5, "New York");
-        phone = new Phone(clocks);
-    }
 
     @Test
-    @DisplayName("Test set time by passing time into it")
+    @DisplayName("Test set time by passing time into it. Will also call setClocksTime")
     void setTime() {
+        Clock clockMock = mock(Clock.class);
+        Clock[] clocks = new Clock[5];
+        for (int i = 0; i < 5; i++) {
+            clocks[i] = clockMock;
+        }
+        Phone phone = new Phone(clocks);
+
+        // test called function in constructor
+        verify(clockMock, times(5)).setTime(any());
+
         Calendar c = Calendar.getInstance();
         phone.setTime(c);
-        assertEquals(phone.getTime().getTime().getTime(), c.getTime().getTime(), "Set phone time error.");
 
-        // Also test the private class
+        // test called function in setTime, it should be called 5 times in this function.
+        assertEquals(phone.getTime().getTime().getTime(), c.getTime().getTime(), "Set phone time error.");
+        verify(clockMock, times(10)).setTime(any());
     }
 }
