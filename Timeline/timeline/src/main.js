cbb52dd414aad08c456d9942fad37f807d1110bd
yueyue200830 <yueyue200830@163.com>
2019-11-14 19:29:49 +0800

    Add test by creating a new frontend project

--- Timeline/timeline/src/main.js
+++ Timeline/timeline/src/main.js
@@ -1,16 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
 import ElementUI from 'element-ui'
-import axios from 'axios'
 import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
 
 Vue.config.productionTip = false;
 
-Vue.use(VueRouter);
 Vue.use(ElementUI);
 Vue.prototype.$http = axios;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app');
+}).$mount('#app')
---
+++ Timeline/timeline/tests/unit/AppTest.spec.js
@@ -0,0 +1,26 @@
+import { mount, createLocalVue } from '@vue/test-utils'
+import App from '@/App.vue'
+import axios from 'axios'
+import ElementUI from 'element-ui'
+
+const localVue = createLocalVue()
+
+// 正常安装插件
+localVue.use(ElementUI)
+localVue.prototype.$http = axios;
+
+describe('App.vue', () => {
+
+  const wrapper = mount(App, {localVue});
+
+  it('renders the correct markup', () => {
+     expect(wrapper.html()).toContain('Timeline')
+  })
+
+  it('has button', () => {
+    expect(wrapper.contains('button.refresh')).toBe(true);
+    expect(wrapper.contains('button.load-more')).toBe(true)
+  })
+
+
+})
